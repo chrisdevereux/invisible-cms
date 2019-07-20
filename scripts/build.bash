9d03a1647a43d04306c0f1be73b796e7d740ac49
Chris Devereux <devereux.chris@gmail.com>
2019-07-20 06:25:29 +0100

    build: disable esmodule output for now

--- scripts/build.bash
+++ scripts/build.bash
@@ -5,16 +5,16 @@ rm packages/*/*.mjs
 rm packages/*/*.map
 rm packages/*/*.d.ts
 
-tsc
+# tsc
 
-for i in packages/*/*.js
-do
-  mv "${i}" "${i/%js/mjs}"
-done
+# for i in packages/*/*.js
+# do
+#   mv "${i}" "${i/%js/mjs}"
+# done
 
-for i in packages/*/*.js.map
-do
-  mv "${i}" "${i/%js.map/mjs.map}"
-done
+# for i in packages/*/*.js.map
+# do
+#   mv "${i}" "${i/%js.map/mjs.map}"
+# done
 
 tsc --module commonjs
